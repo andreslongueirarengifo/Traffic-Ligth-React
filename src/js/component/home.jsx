44333589f2d360fa189e34c5902dc9d0fd9f8c4e
Andres Longueira <andreslongueirarengifo@gmail.com>
2022-12-09 20:18:34 +0000

    First bonus point/ add random Button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
@@ -7,33 +7,42 @@ const Home = () => {
 	const [colorGreen, setSelectGreen]= useState("")
 	const [colorYellow, setSelectYellow]= useState("")
 
-	const changeColor=(ev)=>{
-		const aux = ev.target.id
-		console.log(aux)
-		if(aux === "red"){
+
+	const glowRender=(aux)=>{
+		if(aux == "1"){
 			setSelectRed("selected")
 			setSelectGreen("")
 			setSelectYellow("")
-		}else if (aux === "yellow"){
+		}else if (aux == "2"){
 			setSelectYellow("selected")
 			setSelectGreen("")
 			setSelectRed("")
-		}else if( aux === "green"){
+		}else if( aux == "3"){
 			setSelectGreen("selected")
 			setSelectYellow("")
 			setSelectRed("")
 		}
 	}
+	const changeColor=(ev)=>{
+		const aux = ev.target.id
+		glowRender(aux)
+	
+	}
+	const randomColor=()=>{
+		const aux = Math.floor(Math.random()*4)
+		glowRender(aux)
+	}
 
 
 	return (
-		<div>
+		<div className="d-flex justify-content-center flex-column">
 		<div className="trafficTop"></div>
 			<div className="container">
-				<div onClick={changeColor} id="red" className={`red light ${colorRed}`}></div>
-				<div onClick={changeColor} id="yellow" className={`yellow light ${colorYellow}`}></div>
-				<div onClick={changeColor} id="green" className={`green light ${colorGreen}`}></div>
+				<div onClick={changeColor} id="1" className={`red light ${colorRed}`}></div>
+				<div onClick={changeColor} id="2" className={`yellow light ${colorYellow}`}></div>
+				<div onClick={changeColor} id="3" className={`green light ${colorGreen}`}></div>
 			</div>
+			<button type="button" class="btn btn-dark w-25 m-auto mt-3" onClick={randomColor}>Change Color</button>
 		</div>
 
 	);
